da3d3fc3fbb715a2aa5dd31cf129adce2749697d
matheus <matheuskaluzny1727@gmail.com>
2024-10-07 12:00:05 -0300

    custo

--- src/main/kotlin/Personagem.kt
+++ src/main/kotlin/Personagem.kt
@@ -16,6 +16,20 @@ open class Personagem(
         definirBonusRacial()
     }
 
+    fun calcularCusto(pontos: Int): Int {
+        return when (pontos) {
+            8 -> 0
+            9 -> 1
+            10 -> 2
+            11 -> 3
+            12 -> 4
+            13 -> 5
+            14 -> 7
+            15 -> 9
+            else -> 0
+        }
+    }
+
     fun validarAtributosPersonagem(
         nome: String,
         forca: Int,
